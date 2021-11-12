769d6278cbc105ee86f7e05e8c71e992eec28bd9
Enrique <fpemlktz@gmail.com>
2021-11-12 10:07:31 +0100

    Fase intermedia 3

--- app/src/main/java/com/enrique/juegomatemtico/MainActivity.java
+++ app/src/main/java/com/enrique/juegomatemtico/MainActivity.java
@@ -10,15 +10,17 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    TextView tv;
+    TextView tv,tvr;
     Button btnramdon, btnresult;
     int numero,divisor1,divisor2,divisor3,divisor4,cnt;
     RadioButton rd1, rd2, rd3, rd4, rd5;
+    boolean compDivisibilidad;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv);
+        tvr = (TextView) findViewById(R.id.tvr);
         btnramdon = (Button) findViewById(R.id.btnrandom);
         btnresult = (Button) findViewById(R.id.btnresultado);
         rd1 = (RadioButton) findViewById(R.id.rd1);
@@ -31,7 +33,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 tv.setText(String.valueOf((int) (Math.random() * (1000 - 2000) + 2000)));
                 numero = Integer.parseInt((String) tv.getText());
-
             }
         });
         btnresult.setOnClickListener(new View.OnClickListener() {
@@ -59,10 +60,23 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 if (!rd1.isChecked() && !rd2.isChecked() && !rd3.isChecked() && !rd4.isChecked() && !rd5.isChecked())
-                    Toast.makeText(getApplicationContext(), "No has seleccionado ninguna opc", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "No has seleccionado ninguna opcion", Toast.LENGTH_LONG).show();
 
-                    tv.setText("Error");
             }
         });
+        btnresult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ((cnt > 0) && calcular_divisivilidad(numero, divisor1)) {
+                    tvr.setText("Correcto");
+                } else
+                    tv.setText("Incorrecto");
+            }
+        });
+    }
+    public boolean calcular_divisivilidad(int numero,int divisores){
+        if(numero%divisores==0)
+            return true;
+        else return false;
     }
 }
