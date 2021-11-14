83fd26f2248c2c644bf267301ca4c53ea94e9b4e
Enrique <fpemlktz@gmail.com>
2021-11-14 22:17:06 +0100

    Prueba nok

--- app/src/main/java/com/enrique/juegomatemtico/MainActivity.java
+++ app/src/main/java/com/enrique/juegomatemtico/MainActivity.java
@@ -3,6 +3,7 @@ package com.enrique.juegomatemtico;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -12,9 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     TextView tv,tvr;
     Button btnramdon, btnresult;
-    int numero,divisor1,divisor2,divisor3,divisor4,cnt;
+    int numero,divisor;
     RadioButton rd1, rd2, rd3, rd4, rd5;
-    boolean compDivisibilidad;
+    ImageView Iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         rd3 = (RadioButton) findViewById(R.id.rd3);
         rd4 = (RadioButton) findViewById(R.id.rd4);
         rd5 = (RadioButton) findViewById(R.id.rd5);
+        Iv=(ImageView) findViewById(R.id.imageView);
         btnramdon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,45 +40,15 @@ public class MainActivity extends AppCompatActivity {
         btnresult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (rd1.isChecked()){
-                    divisor1=Integer.parseInt((String)rd1.getText());
-                    cnt++;
-                }
-                if (rd2.isChecked()){
-                    divisor2=Integer.parseInt((String)rd2.getText());
-                    cnt++;
-                }
-                if (rd3.isChecked()){
-                    divisor3=Integer.parseInt((String)rd3.getText());
-                    cnt++;
-                }
-                if (rd4.isChecked()){
-                    divisor4=Integer.parseInt((String)rd4.getText());
-                    cnt++;
-                }
-                if (rd5.isChecked()){
-                    rd5.getText();
-                    cnt++;
-                }
 
                 if (!rd1.isChecked() && !rd2.isChecked() && !rd3.isChecked() && !rd4.isChecked() && !rd5.isChecked())
                     Toast.makeText(getApplicationContext(), "No has seleccionado ninguna opcion", Toast.LENGTH_LONG).show();
+                Iv.setImageResource(R.drawable.nok);
+                }
 
-            }
-        });
-        btnresult.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if ((cnt > 0) && calcular_divisivilidad(numero, divisor1)) {
-                    tvr.setText("Correcto");
-                } else
-                    tv.setText("Incorrecto");
-            }
-        });
+            });
     }
     public boolean calcular_divisivilidad(int numero,int divisores){
-        if(numero%divisores==0)
-            return true;
-        else return false;
+        return numero % divisores == 0;
     }
 }
